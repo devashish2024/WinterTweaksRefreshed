275530b484e8c33b2bda4aae19a66deee318276b
Ashish Agarwal <code.with.aasheesh@gmail.com>
2025-04-11 03:51:20 +0530

    hate u eslint

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-refresh/only-export-components */
+
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
@@ -43,4 +45,4 @@ function Badge({
   );
 }
 
-export { Badge };
+export { Badge, badgeVariants };
